188e47a17a0735ec5ebc7a0b7d8b9ab7312f4d59
NoferBeautiful <beautifulnofer@gmail.com>
2021-01-28 15:39:39 +0300

    before merge

--- scripts/PlatformManager.gd
+++ scripts/PlatformManager.gd
@@ -4,12 +4,18 @@ onready var Game = get_parent()
 onready var Player = Game.get_node("Player")
 var Platform_standart = preload("res://scenes/Platform_standart.tscn")
 var Platform_broken = preload("res://scenes/Platform_broken.tscn")
+var Platform_chipped = preload("res://scenes/Platform_chipped.tscn")
+var Platform_chipped_up = preload("res://scenes/Platform_chipped_up.tscn")
+var Platform_chipped_down = preload("res://scenes/Platform_chipped_down.tscn")
+var Platform_crab = preload("res://scenes/Platform_crab.tscn")
 
-var chance = {"platform_basic" : 0.4, "platform_broken" : 0.1}
+var chance = {"platform_basic" : 0.4, "platform_broken" : 0.2, "platform_chipped" : 0.2}
 var lines = 10
-var columns = 3
-var dist_betw_lines = 50.0
-var dist_betw_columns = 170.0
+var columns = 30
+var dist_betw_lines = 80.0
+var dist_betw_columns = 220.0
+var plat_min = 2
+var plat_max = 7
 var last_plat
 
 
@@ -23,13 +29,15 @@ func generate_section():
 	
 	for i in range(lines):
 		for j in range(columns):
+			section[i][j] = (i+j) % 6 + 1
+			"""
 			if (randf() <= chance["platform_basic"]):
 				continue
 			if (randf() <= chance["platform_broken"]):
 				section[i][j] = 2
 			else:
 				section[i][j] = 1
-	
+			"""
 	return section
 
 
@@ -44,6 +52,14 @@ func place_section():
 				plat = Platform_standart.instance()
 			if (section[i][j] == 2):
 				plat = Platform_broken.instance()
+			if (section[i][j] == 3):
+				plat = Platform_chipped.instance()
+			if (section[i][j] == 4):
+				plat = Platform_chipped_up.instance()
+			if (section[i][j] == 5):
+				plat = Platform_chipped_down.instance()
+			if (section[i][j] == 6):
+				plat = Platform_crab.instance()
 			plat.position = position + Vector2(j * dist_betw_columns, i * dist_betw_lines)
 			Game.add_child(plat)
 
