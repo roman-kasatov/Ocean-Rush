f9bd33ab42f94b64c545d4107c00ca1af6a8c16a
NoferBeautiful <beautifulnofer@gmail.com>
2021-07-06 01:23:06 +0300

    last update on today

--- scripts/GatherArea.gd
+++ scripts/GatherArea.gd
@@ -4,14 +4,15 @@ onready var Particles = preload("res://scenes/particles/particles_bonus_explosio
 onready var Player = get_parent()
 
 func _area_entered(area):
-	if area.is_in_group("gatherable"):
-		var type = area.type
-		if type in ['shield_bonus', 'jump_bonus']:
-			get_parent().add_bonus(type)
-		elif type == 'coin':
-			pass
-		area.queue_free()
-		var particles = Particles.instance()
-		particles.emitting = true
-		particles.position = area.get_global_position()#get_global_position() + Vector2(40, 16)
-		Player.get_parent().add_child(particles)
+	if not(area.is_in_group("gatherable")):
+		return
+	var type = area.type
+	if type in ['shield_bonus', 'jump_bonus']:
+		get_parent().add_bonus(type)
+	elif type == 'coin':
+		pass
+	area.queue_free()
+	var particles = Particles.instance()
+	particles.emitting = true
+	particles.position = area.get_global_position()#get_global_position() + Vector2(40, 16)
+	Player.get_parent().add_child(particles)
