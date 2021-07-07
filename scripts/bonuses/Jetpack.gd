32021485989f6151641c331fd8dff0315b0047fb
NoferBeautiful <beautifulnofer@gmail.com>
2021-07-07 20:01:15 +0300

    bug fixes, coins in progress

--- scripts/bonuses/Jetpack.gd
+++ scripts/bonuses/Jetpack.gd
@@ -1,10 +1,14 @@
 extends Sprite
 
 onready var Particles = preload("res://scenes/particles/particles_jetpack_falling.tscn")
+onready var Jetpack_with_fuel = preload("res://drawable/Character/jetpack_with_fuel.png")
+onready var Jetpack_without_fuel = preload("res://drawable/Character/jetpack.png")
 onready var Player = get_parent()
 
 onready var Fuel = $Fuel
 onready var Fire = $Fire
+
+onready var working = 0
 
 func appear():
 	Fuel.playing = false
@@ -14,7 +18,12 @@ func appear():
 func disappear():
 	if !visible:
 		return
+	working = 0
 	var particles = Particles.instance()
+	if (Fuel.frame == 0):
+		particles.texture = Jetpack_with_fuel
+	else:
+		particles.texture = Jetpack_without_fuel
 	particles.emitting = true
 	particles.position = get_global_position() + Vector2(0, 16)
 	Player.get_parent().add_child(particles)
@@ -25,8 +34,10 @@ func launch():
 		return
 	Fuel.playing = true
 	Fire.emitting = true
+	working = 1
 
 func _on_Fuel_animation_finished():
+	Player.can_pick = 1
 	disappear()
 
 func _on_Fuel_frame_changed():
