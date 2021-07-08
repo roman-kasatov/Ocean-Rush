3dbbcc89a7846d5c5ac4e53df37de129809333fa
NoferBeautiful <beautifulnofer@gmail.com>
2021-07-08 19:31:27 +0300

    updates to scarfs, camera and smth other, merge carefully

--- scripts/Scarf.gd
+++ scripts/Scarf.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var length = 300
+var length_add = 0
 var constrain : float = 5
 var gravity = Vector2(0, 4)
 var friction_x = 0
@@ -16,11 +17,13 @@ var count: int
 func _ready():
 	# texture has height 80 px and width about 150-300
 	#var scarf_texture = load("res://drawable/linear_combination.png")
-	var scarf_texture = load("res://drawable/scarfs/q6.jpg")
+	var scarf_texture = load("res://drawable/scarfs/flag_cnt_bel.png")
 	$Line2D.texture = scarf_texture
 	# 80 is a scarf heigth === width, 
 	# I really don't know why it should be devided by 2
-	length = 80 * scarf_texture.get_size().x / scarf_texture.get_size().y / 2
+	if scarf_texture.get_size().x == 1:
+		length_add = 199
+	length = 80 * scarf_texture.get_size().x / scarf_texture.get_size().y / 2 + length_add
 	
 	count = get_count(length)
 	resize_arrays()
--- scripts/platforms/Platform_chipped.gd
+++ scripts/platforms/Platform_chipped.gd
@@ -5,13 +5,13 @@ var min_speed = 0.3
 var max_speed = 3
 var speed = 0.3
 
-func _ready():
-	update_rate()
+#func _ready():
+#	update_rate()
 
 #func _physics_process(delta):
 #	update_rate()
 #	position.x -= speed
 
-func update_rate():
-	speed = lerp(min_speed, max_speed, Game.rate / 100)
+#func update_rate():
+#	speed = lerp(min_speed, max_speed, Game.rate / 100)
 
