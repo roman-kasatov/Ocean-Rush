16e778d34cfe225a7a74f3193e881a63f70fdb94
roman-kasatov <kasatov.r.e@yandex.ru>
2021-07-09 22:35:55 +0300

    score fixed

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var rate : float = 0
+var score = 0
 onready var Player = $Player
 onready var GameHUD = $GameHUD
 onready var PlatformManager = $PlatformManager
@@ -13,9 +14,11 @@ func _physics_process(delta):
 		rate += 2 * delta
 
 func _ready():
+	Events.connect('start_game', self, 'start_game')
 	$CanvasLayer/Blackout.open()
 	randomize()
 	get_tree().paused = true
+
 
 func fail():
 	get_tree().paused = true
@@ -34,11 +37,22 @@ func reset():
 	get_tree().reload_current_scene()
 	get_tree().paused = false
 
+func inc_score():
+	score += 1
+	$GameHUD/ScorePanel/Score.text = str(score)
+
 func start_game():
-	GameHUD.get_node("ScorePanel/Score").timer.start()
+	print('asdfsad')
+	var score_timer = Timer.new()
+	score_timer.wait_time = 1.0
+	score_timer.one_shot = false
+	score_timer.autostart = true
+	score_timer.connect("timeout", self, 'inc_score')
+	add_child(score_timer)
+	score_timer.start()
+
 	$Player/CPUParticles2D.emitting = true
-	GameHUD.get_node("ScorePanel").visible = true
-	GameHUD.get_node("SkinPanel").visible = false
-	GameHUD.get_node("SkinSwitches").visible = false
+#	GameHUD.get_node("ScorePanel").visible = true
+#	GameHUD.get_node("SkinPanel").visible = false
+#	GameHUD.get_node("SkinSwitches").visible = false
 	PlatformManager.place_section()
-	emit_signal("start_game")
