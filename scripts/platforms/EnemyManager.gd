56cd85471f26a265823bb120f1bf5d7987379a82
NoferBeautiful <beautifulnofer@gmail.com>
2021-08-23 19:54:48 +0300

    finished spawn enemies

--- scripts/platforms/EnemyManager.gd
+++ scripts/platforms/EnemyManager.gd
@@ -3,11 +3,12 @@ extends Node2D
 onready var Game = get_parent()
 onready var Player = Game.get_node("Player")
 
-var score_for_medium = 60
-var score_for_hard = 110
+var last_wave = -1
+var score_for_medium = 1
+var score_for_hard = 1
 
-var enemy_curr = 0
-var can_spawn = 1
+var enemy_curr = 55
+var can_spawn = 105
 
 var shark = preload("res://scenes/enemies/Shark_av.tscn")
 var jellyfish = preload("res://scenes/enemies/Jellyfish_warn.tscn")
@@ -19,13 +20,27 @@ var player_posy = 0
 var enemies_spawn_chance = 0.4
 
 var enemies_easy = [
-	[[0, shark, 1.3], [0, shark, 2.0]]
+	[[0, shark, 1.3], [0, shark, 2.0]],
+	[[0, shark, 1.3], [0, jellyfish, 2.0, 3]],
+	[[0, jellyfish, 1.5, 5]], 
+	[[60, shark, 0.001], [-60, shark, 2.5]],
+	[[-60, jellyfish, 0.001, 2], [60, jellyfish, 2.0, 2]]
 ]
 
 var enemies_medium = [
+	[[60, shark, 1.2], [60, shark, 0.001], [-60, shark, 1.2], [60, shark, 2.8]],
+	[[60, jellyfish, 0.001, 5], [-60, jellyfish, 1.5, 5]],
+	[[0, jellyfish, 2.0, 10]],
+	[[0, submarine, 2.5]],
+	[[0, shark, 0.001], [-60, jellyfish,2.0, 4]]
 ]
 
 var enemies_hard = [
+	[[0, shark, 0.001], [60, shark, 0.001], [-60, shark, 3.0]],
+	[[0, jellyfish, 0.001, 5], [60, jellyfish, 0.001, 5], [-60, jellyfish, 3.0, 5]],
+	[[0, submarine, 1.5], [60, jellyfish, 0.001, 5], [-60, jellyfish, 3.0, 5]],
+	[[0, shark, 1.3], [0, shark, 1.3], [0, shark, 3.0]],
+	[[0, shark, 1.3], [0, jellyfish, 1.5, 7], [0, shark, 3.0]]
 ]
 
 
@@ -45,7 +60,11 @@ func begin_wave():
 func choose_waves(waves):
 	player_posy = Player.position.y
 	can_spawn = 0
-	wave = waves[randi() % len(waves)]
+	var wave_ind = last_wave
+	while wave_ind == last_wave:
+		wave_ind = randi() % len(waves)
+	last_wave = wave_ind
+	wave = waves[wave_ind]
 	next_enemy()
 
 
@@ -55,8 +74,8 @@ func spawn_enemy():
 	if to_spawn[1] == jellyfish:
 		enemy.spawns_left = to_spawn[3]
 	var view_size = get_viewport_rect().size
-	enemy.position = Vector2(0.18 * view_size.x, (randf() - 0.5) * 140 + to_spawn[0])
-	enemy.coord_const = Player.position.y
+	enemy.position = Vector2(0.18 * view_size.x, (randf() - 0.5) * 140)
+	enemy.coord_const = Player.position.y + to_spawn[0]
 	Player.add_child(enemy)
 	return to_spawn[2]
 
